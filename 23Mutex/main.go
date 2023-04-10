4113187b79eb5ce5765de0b56e3d27550da55376
Readytowork co. ltd <readytowork@Readytoworks-MacBook-Air.local>
2023-04-10 21:26:25 +0545

    go mutex

---
+++ 23Mutex/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// mutex are data structure provide by sync package
+// they can help us  a lock on different sections of data so that only goroutine ca access it at a time.
+func main() {
+	fmt.Println("mutex")
+}
