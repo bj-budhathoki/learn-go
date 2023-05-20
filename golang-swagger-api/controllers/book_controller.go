2b3ec6d8287fddde67492b643c58f8ca1663b822
Readytowork co. ltd <readytowork@Readytoworks-MacBook-Air.local>
2023-05-20 11:40:44 +0545

    feat::fire: added more routes

--- golang-swagger-api/controllers/book_controller.go
+++ golang-swagger-api/controllers/book_controller.go
@@ -1,28 +1,68 @@
 package controllers
 
 import (
+	"errors"
 	"net/http"
+	"strconv"
 
-	"github.com/bj-budhathoki/learn-go/golang-swagger-api/models"
 	"github.com/gin-gonic/gin"
 )
 
+type Book struct {
+	ID       int    `json:"id"`
+	Title    string `json:"title"`
+	Author   string `json:"author"`
+	Quantity int    `json:"quantity"`
+}
+
+var books = []Book{
+	{ID: 1, Title: "HTML and CSS: Design and Build Websites", Author: "Jon Duckett", Quantity: 2},
+	{ID: 2, Title: "Coding All-In-One for Dummies", Author: " DK", Quantity: 5},
+}
+
 func GetBooks(ctx *gin.Context) {
-	books := []models.BookModel{
-		{ID: 1, Title: "HTML and CSS: Design and Build Websites", Author: "Jon Duckett", Quantity: 2},
-		{ID: 2, Title: "Coding All-In-One for Dummies", Author: " DK", Quantity: 5},
-	}
 	ctx.IndentedJSON(http.StatusOK, gin.H{
 		"data":    books,
 		"message": "success",
 	})
 }
+
+// create new book
+func CreateBook(ctx *gin.Context) {
+	var newBook Book
+	if err := ctx.BindJSON(&newBook); err != nil {
+		ctx.AbortWithError(500, err)
+	}
+	books = append(books, newBook)
+	ctx.IndentedJSON(http.StatusCreated, newBook)
+}
+
+// get single book by id
+func getBookById(id int) (*Book, error) {
+	for i, b := range books {
+		if b.ID == id {
+			return &books[i], nil
+		}
+	}
+	return nil, errors.New("book not found")
+}
+
 func GetBook(ctx *gin.Context) {
-	book := models.BookModel{
-		ID: 1, Title: "HTML and CSS: Design and Build Websites", Author: "Jon Duckett", Quantity: 2,
+	id, _ := strconv.Atoi(ctx.Param("id"))
+	book, err := getBookById(id)
+	if err != nil {
+		ctx.AbortWithStatus(500)
 	}
 	ctx.IndentedJSON(http.StatusOK, gin.H{
 		"data":    book,
+		"message": "success",
+	})
+}
+
+// delete book
+
+func DeleteBook(ctx *gin.Context) {
+	ctx.IndentedJSON(http.StatusOK, gin.H{
 		"message": "success",
 	})
 }
--- golang-swagger-api/routes/book_route.go
+++ golang-swagger-api/routes/book_route.go
@@ -8,5 +8,7 @@ import (
 func RegisterBookRoutes(router *gin.Engine) {
 	routes := router.Group("/books")
 	routes.GET("/", controllers.GetBooks)
+	routes.POST("/", controllers.CreateBook)
 	routes.GET("/:id", controllers.GetBook)
+	routes.DELETE("/:id", controllers.DeleteBook)
 }
